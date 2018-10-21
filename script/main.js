b4bbb7521fb36f7d074a4e252fe50bb0b44ff777
startx13 <theleo103@gmail.com>
2018-10-21 23:00:20 +0200

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -3,17 +3,40 @@ function execProgram()
 	var program = document.getElementById("program").value;
 	console.log(program);
 	console.log(typeof(program));
+	var pArray = programToArray(program);
+	var i,j;
+	for(i=0;i<pArray.length;i++)
+	{
+		console.log(pArray[i]);
+		if(pArray[i][0] == undefined)
+			break;
+	}	
+	
 }
 
 function programToArray(program)
 {
 	var i;
-	var parsedProgram;
+	var j = 0;
+	var parsedProgram = new Array(100);
+
+	for(i=0;i<parsedProgram.length;i++)
+	{
+			parsedProgram[i] = new Array(30);
+	}	
+
 	for(i=0;i<program.length;i++)
 	{
-		if(program[i] == "\n")
+		if(program[i] == " ")
 		{
-		
+			j++;			
+		}
+		else
+		{
+			for(i=0;i<parsedProgram[j].length;i++)
+			{			
+				parsedProgram[j][i] = program[i];	
+			}		
 		}
 	}
 	return parsedProgram;
